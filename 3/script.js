771887acee909efd97211da98df67c2f3ebad48e
clerickbarrion <clerickbarrion@gmail.com>
2023-11-08 14:42:46 -0500

    fixed delays

--- 3/script.js
+++ 3/script.js
@@ -4,24 +4,51 @@ function hello(time){
 
 function debounce(calls, t) {
     output = []
-    calls.forEach(call => {
+    for (let i = 0; i < calls.length; i++){
+
         timeout = setTimeout(() => {
-            hello(call.t)
-        }, call.t)
-        
-        if (call.t <= t) {
-            clearTimeout(timeout)
-        } else {
-            call.t += t
-            output.push(call)
+            hello(calls[i].t)
+        }, calls[i].t + t)
+
+        if (!calls[i+1]) {
+            calls[i].t += t
+            output.push(calls[i])
+            return output
         }
-    })
+
+        if ((calls[i+1].t - calls[i].t < t)) {
+            clearTimeout(timeout)
+        }
+        else {
+            calls[i].t += t
+            output.push(calls[i])
+        }
+    }
     return output
 }
 
-const calls = [
-    {"t":  50, inputs: [1]},
-    {"t": 75, inputs: [2]}
-]
+let calls = [
+    {"t": 50, inputs: [1, 2]},
+    {"t": 300, inputs: [3, 4]},
+    {"t": 300, inputs: [5, 6]}
+  ]
+  
+let t = 150
+console.log(debounce(calls, t))
 
-console.log(debounce(calls, 50))
+calls = [
+    {"t": 50, inputs: [1]},
+    {"t": 100, inputs: [2]}
+  ]
+  
+ t = 20
+console.log(debounce(calls, t))
+
+calls = [
+    {"t": 50, inputs: [1]},
+    {"t": 75, inputs: [2]}
+  ]
+  
+
+t = 50
+console.log(debounce(calls, t))
