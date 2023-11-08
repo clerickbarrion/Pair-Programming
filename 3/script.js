1a162d69b5edbe234d00714711370a3f3ca4b4bf
clerickbarrion <clerickbarrion@gmail.com>
2023-11-08 13:26:20 -0500

    completed 3

---
+++ 3/script.js
@@ -0,0 +1,28 @@
+function hello(){
+    console.log('Hello')
+}
+
+function debounce(calls, t) {
+    output = []
+    calls.forEach(call => {
+        timeout = setTimeout(() => {
+            hello()
+        }, t)
+        
+        if (call.t <= t) {
+            clearTimeout(timeout)
+        } else {
+            call.t += t
+            output.push(call)
+        }
+    })
+    return output
+}
+
+const calls = [
+    {"t":  50, inputs: [1]},
+    {"t": 75, inputs: [2]}
+]
+
+console.log(debounce(calls, 50))
+
